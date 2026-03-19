5fe616822f5db2ab680d55927a1dfa1548e59fd2
Samuele Ferri <samuele1301@gmail.com>
2026-03-19 10:52:44 +0100

    feat: takeWhile done

--- src/main/scala/it/unibo/pps/u03/Streams.scala
+++ src/main/scala/it/unibo/pps/u03/Streams.scala
@@ -1,5 +1,7 @@
 package u03
 
+import scala.collection.View.TakeWhile
+
 object Streams extends App :
 
   import Sequences.*
@@ -37,16 +39,25 @@ object Streams extends App :
     def iterate[A](init: => A)(next: A => A): Stream[A] =
       cons(init, iterate(next(init))(next))
 
+    def takeWhile[A](stream: Stream[A])(pred: A => Boolean): Stream[A] = stream match
+      case Cons(head, tail) if pred(head()) => cons(head(), takeWhile(tail())(pred))
+      case _ => Empty()
+
+
   end Stream
 
 @main def tryStreams =
   import Streams.* 
 
-  val str1 = Stream.iterate(0)(_ + 1) // {0,1,2,3,..}
-  val str2 = Stream.map(str1)(_ + 1) // {1,2,3,4,..}
-  val str3 = Stream.filter(str2)(x => (x < 3 || x > 20)) // {1,2,21,22,..}
-  val str4 = Stream.take(str3)(10) // {1,2,21,22,..,28}
-  println(Stream.toList(str4)) // [1,2,21,22,..,28]
+//  val str1 = Stream.iterate(0)(_ + 1) // {0,1,2,3,..}
+//  val str2 = Stream.map(str1)(_ + 1) // {1,2,3,4,..}
+//  val str3 = Stream.filter(str2)(x => (x < 3 || x > 20)) // {1,2,21,22,..}
+//  val str4 = Stream.take(str3)(10) // {1,2,21,22,..,28}
+//  println(Stream.toList(str4)) // [1,2,21,22,..,28]
+//
+//  lazy val corec: Stream[Int] = Stream.cons(1, corec) // {1,1,1,..}
+//  println(Stream.toList(Stream.take(corec)(10))) // [1,1,..,1]
 
-  lazy val corec: Stream[Int] = Stream.cons(1, corec) // {1,1,1,..}
-  println(Stream.toList(Stream.take(corec)(10))) // [1,1,..,1]
+  val stream = Stream.iterate(0)(_ + 1)
+  println(Stream.toList(Stream.takeWhile(stream)(_ < 5)))
+  // Cons (0 , Cons (1 , Cons (2 , Cons (3 , Cons (4 , Nil ())))))
